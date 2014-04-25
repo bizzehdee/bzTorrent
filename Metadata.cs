a82e48e429b66b8bb9ec8099de2d05cfc9a6b0ea
Darren Horrocks <killallthehumans@gmail.com>
2014-04-25 21:55:44 +0100

    updated Metadata parsing and added MagnetLink parsing.

--- Metadata.cs
+++ Metadata.cs
@@ -1,7 +1,38 @@
-﻿using System.Collections.Generic;
+﻿/*
+Copyright (c) 2013, Darren Horrocks
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+* Neither the name of Darren Horrocks nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+*/
+
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Net.Torrent.bencode;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace System.Net.Torrent
@@ -10,7 +41,15 @@ namespace System.Net.Torrent
     {
         private IBencodingType _root;
 
-        public String Comment { get; set; }
+	    public byte[] Hash { get; set; }
+
+	    public String HashString
+	    {
+		    get { return Unpack.Hex(Hash); }
+		    set { Hash = Pack.Hex(value); }
+	    }
+
+	    public String Comment { get; set; }
         public String Announce { get; set; }
         public ICollection<String> AnnounceList { get; set; }
         public String CreatedBy { get; set; }
@@ -23,19 +62,47 @@ namespace System.Net.Torrent
 
         public Metadata()
         {
-            AnnounceList = new Collection<string>();
-            PieceHashes = new Collection<byte[]>();
-			Files = new Dictionary<string, long>();
+	        Init();
         }
 
         public Metadata(Stream stream)
         {
-            AnnounceList = new Collection<string>();
-            PieceHashes = new Collection<byte[]>();
-			Files = new Dictionary<string, long>();
+	        Init();
 
             Load(stream);
         }
+
+	    public Metadata(MagnetLink magnetLink)
+	    {
+		    Init();
+
+		    Load(magnetLink);
+	    }
+
+	    private void Init()
+	    {
+			AnnounceList = new Collection<string>();
+			PieceHashes = new Collection<byte[]>();
+			Files = new Dictionary<string, long>();
+	    }
+
+	    public bool Load(MagnetLink magnetLink)
+	    {
+		    if (magnetLink == null) return false;
+		    if (magnetLink.Hash == null) return false;
+
+		    HashString = magnetLink.HashString;
+
+		    if (magnetLink.Trackers != null)
+		    {
+			    foreach (string tracker in magnetLink.Trackers)
+			    {
+				    AnnounceList.Add(tracker);
+			    }
+		    }
+
+		    return true;
+	    }
 
         public bool Load(Stream stream)
         {
@@ -93,6 +160,12 @@ namespace System.Net.Torrent
             if (dictRoot.ContainsKey("info"))
             {
                 BDict infoDict = (BDict)dictRoot["info"];
+
+	            using (SHA1Managed sha1 = new SHA1Managed())
+	            {
+					byte[] str = BencodingUtils.EncodeBytes(infoDict);
+					Hash = sha1.ComputeHash(str);
+	            }				
 
 	            if (infoDict.ContainsKey("files"))
 	            {
